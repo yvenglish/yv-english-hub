acfed3754dfd69ec65bacce4d7741de2c36452bc
yvenglish <yasminvizeusantana@yahoo.com>
2026-07-16 14:45:50 -0300

    Remove hardcoded library priority to allow chronological sort

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.33fn69bh2q4"
+    "revision": "0.73rrtb2afc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -77,14 +77,7 @@ export default function Library() {
       return dateB - dateA;
     });
 
-    const priority = ["bad habits", "rapunzel", "introduc", "dua lipa", "communication", "toy story 5", "23 verbos"];
-    result = result.map(ep => {
-      const title = (ep.title || '').toLowerCase();
-      let idx = priority.findIndex(p => title.includes(p));
-      return { ...ep, _priorityIdx: idx === -1 ? 999 : idx };
-    });
-
-    return result.sort((a, b) => a._priorityIdx - b._priorityIdx);
+    return result;
   }, [episodes, search, filter, favorites]);
 
   if (selectedEpisode) {
